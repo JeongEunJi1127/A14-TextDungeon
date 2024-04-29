c2ead57fc3bb1ffe82414b8444895841a51b3a34
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-04-29 12:12:42 +0900

    Init : gamemanager

--- A14-TextDungeon/A14-TextDungeon/Manager/GameManager.cs
+++ A14-TextDungeon/A14-TextDungeon/Manager/GameManager.cs
@@ -9,9 +9,9 @@ namespace A14_TextDungeon.Manager
         {
             User user = new User("UserName",1,"전사", 10, 5, 100, 1500, false );
 
-            Monster minion = new Monster("미니언", 2, 1, 15, false );
-            Monster siegeMinion = new Monster("미니언", 2, 1, 15, false);
-            Monster vacuity = new Monster("미니언", 2, 1, 15, false);
+            Monster minion = new Monster("미니언", 2, 5, 15, false );
+            Monster vacuity = new Monster("공허충", 3, 9, 10, false);
+            Monster siegeMinion = new Monster("대포미니언 ", 5, 8, 25, false);
         }
         static void Main(string[] args)
         {
