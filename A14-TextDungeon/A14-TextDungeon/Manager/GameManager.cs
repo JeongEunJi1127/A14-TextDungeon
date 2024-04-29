4f867d3910d5a46d1aadd5413121db5721e094d3
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-04-29 12:04:06 +0900

    init

--- A14-TextDungeon/A14-TextDungeon/Manager/GameManager.cs
+++ A14-TextDungeon/A14-TextDungeon/Manager/GameManager.cs
@@ -1,9 +1,18 @@
-﻿using A14_TextDungeon.Scene;
+﻿using A14_TextDungeon.Data;
+using A14_TextDungeon.Scene;
 
 namespace A14_TextDungeon.Manager
 {
     internal class GameManager
     {
+        public void  Init()
+        {
+            User user = new User("UserName",1,"전사", 10, 5, 100, 1500, false );
+
+            Monster minion = new Monster("미니언", 2, 1, 15, false );
+            Monster siegeMinion = new Monster("미니언", 2, 1, 15, false);
+            Monster vacuity = new Monster("미니언", 2, 1, 15, false);
+        }
         static void Main(string[] args)
         {
             Village.ShowVillage();
