ef53adccd9aba7ef07d22d98403ef36c2ef7cb78
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-04-30 20:40:05 +0900

    Refactor

--- A14-TextDungeon/A14-TextDungeon/Data/Item.cs
+++ A14-TextDungeon/A14-TextDungeon/Data/Item.cs
@@ -1,15 +1,15 @@
 ﻿using System;
+using A14_TextDungeon.Scene;
 
-namespace A14_TextDungeon.Scene
+namespace A14_TextDungeon.Data
 {
     public class Item
     {
-        public string ItemName;
-        public string ItemDescription;
-        public ItemType ItemType;
-        public int ItemStat;
-        public bool IsEquippd;
-
+        public string ItemName { get; private set; }
+        public string ItemDescription { get; private set; }
+        public ItemType ItemType { get; private set; }
+        public int ItemStat { get; private set; }
+        public bool IsEquippd { get;  set; }
 
         public Item(string name, int stat, ItemType type, string desription, bool isEquippd = false)
         {
@@ -43,4 +43,4 @@ namespace A14_TextDungeon.Scene
         }
     }
 }
-  
+
