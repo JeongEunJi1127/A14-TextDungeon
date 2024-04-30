cc4cae49935feb8745d5168ef304026a5f3e6507
dldlsgh97 <45443021+dldlsgh97@users.noreply.github.com>
2024-04-30 11:12:31 +0900

    Refactor/Item.cs분리

--- A14-TextDungeon/A14-TextDungeon/Scene/Inventory.cs
+++ A14-TextDungeon/A14-TextDungeon/Scene/Inventory.cs
@@ -82,46 +82,5 @@ namespace A14_TextDungeon.Scene
                 }
             }
         }
-    }
-
-    
-
-    public class Item
-    {
-        public string ItemName;
-        public string ItemDescription;
-        public ItemType ItemType;
-        public int ItemStat;
-
-        
-
-        public Item(string name,int stat,ItemType type,string desription)
-        {
-            ItemName = name;
-            ItemDescription = desription;
-            ItemType = type;
-            ItemStat = stat;
-        }
-
-        public string DisplayItem()
-        {
-            string type;
-            switch (ItemType)
-            {
-                case ItemType.Armor:
-                    type = "방어력 +";
-                    break;
-                case ItemType.Wepon:
-                    type = "공격력 +";
-                    break;
-                case ItemType.Potion:
-                    type = "HP회복 +";
-                    break;
-                default:
-                    type = "";
-                    break;
-            }
-            return $"{ItemName} | {type}{ItemStat} | {ItemDescription}";
-        }
-    }
+    }   
 }
